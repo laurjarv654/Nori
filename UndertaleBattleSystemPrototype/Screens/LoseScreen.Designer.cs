4a9a00ef91a38fd8ab9becd0a3ee4649d4347c25
Christopher Phillis <ChrisPhillis@outlook.com>
2020-11-11 12:24:18 -0500

    V0.3 - MenuScreen and LoseScreen are both done. ControlsScreen needs to be done, music and sounds need to be added still.

--- UndertaleBattleSystemPrototype/Screens/LoseScreen.Designer.cs
+++ UndertaleBattleSystemPrototype/Screens/LoseScreen.Designer.cs
@@ -28,20 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.titleSprite = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.titleSprite)).BeginInit();
             this.SuspendLayout();
+            // 
+            // titleSprite
+            // 
+            this.titleSprite.BackgroundImage = global::UndertaleBattleSystemPrototype.Properties.Resources.gameOverSprite;
+            this.titleSprite.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.titleSprite.Location = new System.Drawing.Point(32, 100);
+            this.titleSprite.Name = "titleSprite";
+            this.titleSprite.Size = new System.Drawing.Size(880, 150);
+            this.titleSprite.TabIndex = 1;
+            this.titleSprite.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
             // LoseScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Black;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.titleSprite);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "LoseScreen";
-            this.Size = new System.Drawing.Size(958, 719);
+            this.Size = new System.Drawing.Size(944, 681);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.LoseScreen_Paint);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.LoseScreen_KeyUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.LoseScreen_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.titleSprite)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox titleSprite;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
